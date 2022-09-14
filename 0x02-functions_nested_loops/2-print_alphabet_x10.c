2c4a0391a85b4bf055ed807af4df569367d54ac0
iKingDIDI <106996755+iKingDIDI@users.noreply.github.com>
2022-09-14 13:14:39 +0100

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
-* print_alphabet_x10 - prints the lowercase alphabet 10 times on new lines
-*
-* Return: void 
-*/
+ * print_alphabet_x10 - prints the lowercase alphabet 10 times on new lines
+ *
+ * Return: void
+ */
 
 void print_alphabet_x10(void)
 {
@@ -14,9 +14,7 @@ void print_alphabet_x10(void)
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 26; j++)
-{
 		{
-			
 			_putchar(c + j);
 		}
 		_putchar(10);
