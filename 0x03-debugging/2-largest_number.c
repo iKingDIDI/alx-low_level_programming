693e357eec1887e56dc79af3b32b611b5de9ae89
iKingDIDI <106996755+iKingDIDI@users.noreply.github.com>
2022-09-17 11:03:08 +0100

    2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,13 +10,19 @@
 
 int largest_number(int a, int b, int c)
 {
-	int largest = a;
+	int largest;
 
-	if (largest < b)
+	if (a > b && b > c)
 	{
+		largest = a;
+	}
+	 else if (b > c)
+	 {
 		largest = b;
-		if (largest < c)
-			largest = c;
+	 }
+	else
+	{
+		largest = c;
 	}
 
 	return (largest);
