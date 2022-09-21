fcc4074b33786ab33d533908c8ee3695380d00be
iKingDIDI <nobledidi@yahoo.com>
2022-09-21 02:00:01 -0700

    1. strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+ * _strncat - function to concatenate two strings and return a new string
+ * with n elements of the appended string
+ *
+ * @dest: string 1
+ * @src: string 2
+ * @n: number of chars to include from appended string
