cfd8e620892070905b54ab89f5badb22d62f6093
iKingDIDI <106996755+iKingDIDI@users.noreply.github.com>
2022-09-27 09:55:44 +0100

    6. Chess is mental torture

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -2,7 +2,7 @@
 
 /**
  * print_chessboard - prints the chessboard
- * 
+ *
  * @a: 2-dimensional array
  * Return: void
  */
@@ -12,8 +12,10 @@ void print_chessboard(char (*a)[8])
 
 	for (i = 0; i < 8; i++)
 	{
-		_putchar(a[i][j]);
-	}
-	_putchar('\n');
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
 	}
 }
